c7661008acd342c7de77189eee8246f78e16f24c
Spookyorange <82583304+spookyorange@users.noreply.github.com>
2024-04-26 16:48:09 +0300

    add variables to uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,9 @@
+literal_name_of_installation_directory=".tarball-installations"
+general_installation_directory="$HOME/$literal_name_of_installation_directory"
+postman_installation_directory="$general_installation_directory/postman"
+
 echo "Removing the installation from your home"
-rm -rf $HOME/.tarball-installations/postman
+rm -rf $postman_installation_directory
 rm $HOME/.local/bin/postman
 rm $HOME/.local/share/applications/postman.desktop
 echo "Uninstallation is successfull"
